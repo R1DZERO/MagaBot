fd7a0ccd9e70ee09b6cb4b9e5bf94fd229af2aa9
R1DZERO <omat.nasukhov@gmail.com>
2024-02-21 19:07:03 +0300

    I optimized the code

--- src/Commands.php
+++ src/Commands.php
@@ -5,11 +5,20 @@ namespace MagaBot;
 
 class Commands
 {
+    private array $trueCommand;
 
+    public function __construct()
+    {
+        $this->trueCommand['!show date'] = 'showDate';
+        $this->trueCommand['!show time'] = 'showTime';
+        $this->trueCommand['!show content'] = 'showContent';
+        $this->trueCommand['!help'] = 'help';
+    }
     public function execute(string $command): string
     {
-        if (method_exists($this, $command)) {
-            return $this->{$command}();
+        if (isset($this->trueCommand[$command]))
+        {
+            return $this->{$this->trueCommand[$command]}();
         }
         return 'Unknown command. Write !help to see list of available commands.';
     }
@@ -21,13 +30,17 @@ class Commands
     {
         return date('H:i');
     }
-    private function showContentLink():string
+    private function showContent()
     {
-        return 'https://i0.wp.com/www.technollama.co.uk/wp-content/uploads/2021/08/pepe.png?fit=1200%2C626&ssl=1';
+        return readfile(__DIR__ .'/pepe.webp');
     }
     private function help():string
     {
-        return 'All command list: showTime, showDate, showContentLink';
+        $commandList = [];
+        foreach($this->trueCommand as $command => $method){
+            $commandList[] = $command ;
+        }
+        return 'All command list: ' . implode(', ' , $commandList);
     }
 }
 //сделать так, чтоб команды были одним текстом, а функции в классе другим
